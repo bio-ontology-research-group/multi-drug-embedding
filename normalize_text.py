14d7f9a5279ade19e04745d916af254a6e47496f
monaalsh <mona.alshahrani99@gmail.com>
2018-07-20 09:51:44 +0300

    steps

--- normalize_text.py
+++ normalize_text.py
@@ -1,15 +1,16 @@
 import pdb
 import json
 
-data_dir = '../../Documents/drugs_data/'
+data_dir = 'data/'
 abstract = []
 terms_dict = {}		
 
 
+with open(data_dir+'mesh2doid.dict') as f:
+	mesh2doid = json.load(f)
 
-with open(data_dir+'mesh2omim.dict') as f:
-	mesh2omim = json.load(f)
-
+with open(data_dir+'omim2doid.dict') as f:
+	omim2doid = json.load(f)
 
 with open(data_dir+'chemical_map.dict') as f:
 	chemical_map = json.load(f)
@@ -17,14 +18,14 @@ with open(data_dir+'chemical_map.dict') as f:
 
 count_abstracts = 0
 count_dis = 0
-dis = []
+diseases = []
 genes = []
 human_genes = []
 chem = []
 count_gene = 0
 count_chem = 0
 mapped_chem = []
-with open(data_dir+'medline_abstracts_mapped_drugsRepo.txt','w') as file1:
+with open(data_dir+'medline_abstracts_mapped_drugsrepoI.txt','w') as file1:
 	with open('../../Documents/text_graph/bioconcepts2pubtator_offsets') as f:
 		for line in f:
 			line = line.strip()
@@ -37,8 +38,19 @@ with open(data_dir+'medline_abstracts_mapped_drugsRepo.txt','w') as file1:
 				else:
 					toks = line.strip().split('\t')
 					if toks[4] == 'Disease' and len(toks) > 5:
-						terms_dict[toks[3]] = '<http://bio2vec.net/disease/'+toks[5]+'>'
-						dis.append(toks[5])
+						dis = toks[5]
+						if dis.startswith('MESH:'):
+							mesh = dis.strip('MESH:')
+							if mesh in mesh2doid:
+								doid = mesh2doid[mesh]
+								terms_dict[toks[3]] = '<http://purl.obolibrary.org/obo/'+doid+'>'
+								diseases.append(doid)
+						if dis.startswith('OMIM:'):
+							if dis in omim2doid:
+								doid = omim2doid[dis]
+								terms_dict[toks[3]] = '<http://purl.obolibrary.org/obo/'+doid+'>'
+								diseases.append(doid)
+
 					elif toks[4] == 'Chemical' and len(toks) > 5:
 						mesh = toks[5].strip('MESH:')
 						chem.append(mesh)
@@ -58,9 +70,9 @@ with open(data_dir+'medline_abstracts_mapped_drugsRepo.txt','w') as file1:
 						if it in terms_dict:
 							term = terms_dict[it]
 							newitem.append(term)
-							if 'http://bio2vec.net/disease/' in term:
+							if 'http://purl.obolibrary.org/obo/' in term:
 								count_dis = count_dis+1
-							if 'http://bio2vec.net/chem/' in term:
+							if 'http://bio2vec.net/CID' in term:
 								count_chem = count_chem+1
 							if 'http://www.ncbi.nlm.nih.gov/gene/' in term:
 								count_gene = count_gene+1
@@ -77,7 +89,7 @@ print 'number of abstracts is: {}\n'.format(count_abstracts/2)
 print 'number of disease mentions:{}\n'.format(count_dis)
 print 'number of gene mentions:{}\n'.format(count_gene)
 print 'number of chemicals mentions:{}\n'.format(count_chem)
-print 'number of distinct diseases:{}\n'.format(len(set(dis)))
+print 'number of distinct diseases:{}\n'.format(len(set(diseases)))
 print 'number of distinct chemicals in mesh:{}\n'.format(len(set(chem)))
 print 'number of distinct chemicals in stitch:{}\n'.format(len(set(mapped_chem)))
 print 'number of distinct genes:{}\n'.format(len(set(genes)))
